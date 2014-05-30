6192e5cc4d46b51d70ce134d01d150e20f69e104
Jakub CubaPP Urbanec <jakub.urbanec@gmail.com>
2014-05-30 21:26:15 +0200

    new stats given

--- stat/stat1.pl
+++ stat/stat1.pl
@@ -22,15 +22,45 @@ foreach $row (0..@filename-1){
 	$ID = $filename[$row][2];
 	$klic = $month." ".$date;
 	if ($klic ne $starejklic && $starejklic){
-		$count_f=0;
-		foreach $f ( keys %denni_pole ) {
- 		    print "$starejklic: $f: @{ $denni_pole{$f} }\n";
-		    $count_f++;
- 		}
-		print "Za datum $starejklic zpracovano $count_f uzivatelu\n";
-		undef %denni_pole;
+		&zpracuj_den_uzivatele();
 	}
 	push @{ $denni_pole{$ID} }, $uxtime;
 	$starejklic = $klic;
 }
 
+# je treba vyhodnotit>
+## 1. jestli pocet stisku tlacitka je lichy, pak hlasi problem a udelat zakladni odecet
+## 2. jestli je pocet stisku tlacitka mensi nez 3, pak nelze hodnotit
+## 3. jeslit je rozdil mezi stisky mensi nez $DELTA (10s) pak druhou hodnotu smazat
+
+# pro 1 den 1 usera potrebuju
+# - ID, odecet casu, identifikaci_OK
+
+sub zpracuj_den_uzivatele ()
+{
+	%tempole = %denni_pole;	
+	$dnesni_datum = $starejklic;
+	$count_user = 0;
+	@docasne_pole = ();
+	foreach $user ( keys %tempole ) {
+		$stav = 0;
+		#pocet zaznamu v poli jednoho user dany den
+		$pocetmu= keys $tempole{$user};
+		#sanace 
+		if ($pocetmu < 3) {
+			$stav = "malo vstupu - " . $pocetmu;
+		}
+
+		elsif ( 0 != $pocetmu % 2){
+			$stav = "lichy vstup - " . $pocetmu;	
+		}
+		foreach $uxt (values $tempole{$user}){
+			print "TESTUJU: $uxt ...";
+		}
+ 	    	print "$dnesni_datum: $user: $pocetmu ZAZN. RET: $stav : @{ $tempole{$user} }\n";
+		$count_user++;
+ 	}
+	print "Za datum $dnesni_datum zpracovano $count_user uzivatelu\n";
+        undef %tempole;
+
+}
